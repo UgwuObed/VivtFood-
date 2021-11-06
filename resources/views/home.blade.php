4f629e62f753f60e4b115d82f6ebab40cd466d9d
Ugwu Obed <vjazzyvictor@gmail.com>
2021-11-06 13:34:23 +0100

    fixed login problem

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -89,7 +89,10 @@ https://templatemo.com/tm-558-klassy-cafe
                                 @if (Route::has('login'))
                                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                                     @auth
-                                    <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+                                    <li> <x-app-layout>
+  
+                                    </x-app-layout>
+                                 </li>
                                     @else
                             <li> <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a> </li>
 
